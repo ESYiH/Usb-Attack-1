114b0407f1437e9039232fa373f0fbffb956c9c4
Efus <57728213+EfusRyuga@users.noreply.github.com>
2021-03-28 17:35:02 +0200

    Update DecryptUSB.bat

--- add-on/DecryptUSB.bat
+++ add-on/DecryptUSB.bat
@@ -1,11 +1,11 @@
 @echo off
 cd /D I:\
-ren language_de_prog language_de_prog.pdf
-ren emo1 emo1.png
-ren emo2 emo2.png
-ren emo3 emo3.png
-ren emo4 emo4.png
-ren Diapo_HG Diapo_HG.odp
-ren ring_of_gyges ring_of_gyges.odp
-ren 87d89e71a1fb5413bb363b409ff0fa10 87d89e71a1fb5413bb363b409ff0fa10.jpg
+ren 
+ren 
+ren 
+ren 
+ren 
+ren 
+ren 
+ren 
 exit
