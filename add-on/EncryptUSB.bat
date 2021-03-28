a11c7686230bb1fd210d510e777c19c241ea639d
Efus <57728213+EfusRyuga@users.noreply.github.com>
2021-03-28 17:34:29 +0200

    Update EncryptUSB.bat

--- add-on/EncryptUSB.bat
+++ add-on/EncryptUSB.bat
@@ -1,11 +1,11 @@
 @echo off
 cd /D I:\
-ren language_de_prog.pdf language_de_prog
-ren emo1.png emo1
-ren emo2.png emo2
-ren emo3.png emo3
-ren emo4.png emo4
-ren Diapo_HG.odp Diapo_HG
-ren ring_of_gyges.odp ring_of_gyges
-ren 87d89e71a1fb5413bb363b409ff0fa10.jpg 87d89e71a1fb5413bb363b409ff0fa10
+ren file1 file1.extension
+ren file2 file2.extension
+ren file3 file3.extension
+ren file4 file4.extension
+ren file5 file5.extension
+ren file6 file6.extension
+ren file7 file7.extension
+ren file8 file8.extension
 exit
