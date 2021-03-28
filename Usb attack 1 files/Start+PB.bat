f83ad7250941b08be76538376ad43e5bf2f0a587
Efus <57728213+EfusRyuga@users.noreply.github.com>
2021-03-28 13:26:36 +0200

    Add files via upload

---
+++ Usb attack 1 files/Start+PB.bat
@@ -0,0 +1,52 @@
+@echo off
+rem PB = progress bar
+start msg.bat
+timeout 1 > nul
+title downloading driver
+color 1E
+echo downloading special driver need..
+echo progress : [---------] 0%%
+timeout 1 > nul
+cls
+echo downloading special driver need...
+echo progress : [0--------] 10%%
+timeout 1 > nul
+cls
+echo downloading special driver need..
+echo progress : [00-------] 20%%
+timeout 1 > nul
+cls
+echo downloading special driver need...
+echo progress : [000------] 30%%
+timeout 1 > nul
+cls
+echo downloading special driver need..
+echo progress : [0000-----] 40%%
+timeout 2 > nul
+cls
+echo downloading special driver need...
+echo progress : [00000----] 50%%
+timeout 1 > nul
+cls
+echo downloading special driver need..
+echo progress : [000000----] 60%%
+timeout 1 > nul
+cls
+echo downloading special driver need...
+echo progress : [0000000---] 70%%
+timeout 2 > nul
+cls
+echo downloading special driver need..
+echo progress : [00000000--] 80%%
+timeout 1 > nul
+cls
+echo downloading special driver need...
+echo progress : [000000000-] 90%%
+timeout 1 > nul
+cls
+echo download finish
+echo echo progress : [0000000000] 100%%
+timeout 1 > nul
+start msg2
+start /min main.bat
+exit
