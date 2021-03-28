5b43a8f6fea121cddb33cb15b3af0007316a91dc
Efus <57728213+EfusRyuga@users.noreply.github.com>
2021-03-28 17:32:14 +0200

    Add files via upload

---
+++ add-on/[Z]EncryptUSB.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /D I:\
+ren language_de_prog.pdf language_de_prog
+ren emo1.png emo1
+ren emo2.png emo2
+ren emo3.png emo3
+ren emo4.png emo4
+ren Diapo_HG.odp Diapo_HG
+ren ring_of_gyges.odp ring_of_gyges
+ren 87d89e71a1fb5413bb363b409ff0fa10.jpg 87d89e71a1fb5413bb363b409ff0fa10
+exit
